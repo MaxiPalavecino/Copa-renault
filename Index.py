c4414c828c7738dd3133b8c490c1307bc13238f6
Maxi Palavecino <mpb71677@gmail.com>
2024-11-15 23:34:07 -0300

    Arreglos!!!
    Falta agregar pdf reglamentos en inicio, pdf deportes en fixtures y arreglar parte sponsors porque no esta como lo planeado.

--- Index.py
+++ Index.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template 
-
 app = Flask(__name__)
 
 @app.route('/')
 def principal():
-    return render_template('menu.html')
+    return render_template('Inicio.html')
 
 @app.route('/Cantina')
 def Cantina():
     return render_template('Cantina.html')
 
-@app.route('/Fixture')
+@app.route('/Fixtures')
 def Fixture():
-    return render_template('Fixture.html')
+    return render_template('Fixtures.html')
+
+@app.route('/Inscripcion')
+def Inscripcion():
+    return render_template('Inscripcion.html')
+
+@app.route('/Mapa')
+def Mapa():
+  return render_template('Mapa.html')
+
+@app.route('/Sponsors')
+def Sponsors():
+  return render_template('Sponsors.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3500)
+    app.run(debug=True, port=5000)
